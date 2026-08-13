72ea6290a5690e89e4717b6e2725de19a66b00aa
Poojii001 <poojapal5781@gmail.com>
2026-08-13 16:50:29 +0530

    Add Netlify URL to CORS

--- main.py
+++ main.py
@@ -22,7 +22,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","http://localhost:5174"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://chat-frontend-pooja.netlify.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
